1e09d6ee1021ebebfade1aad6af374c15323a1de
NyergesC <cintia.nyerges@gmail.com>
2022-03-03 17:10:43 +0100

    afa

--- frontend/script.js
+++ frontend/script.js
@@ -1,87 +1,76 @@
-function init (){
-    var a =  50;
-    var b = 6;
 
-    console.log(c) // undifined lesz ha most lefuttatjuk
+function loadEvent () {
 
-    if (a < b) {
-       function initC() { //itt lekeretezzuk a var scopjeat mert local scopeja van
-            var c = b - a;
-       };
-       initC ();
-    } else {
-        function initC() {
-        var c = a - b;
-        };
-        initC();
+    const rootElement= document.getElementById("root")
 
-    };
+    rootElement.insertAdjacentHTML("beforeend", `
+    <section>
+        <form class="container">
+            <div class="label">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="input1" placeholder="Character's name"> 
+                <p id="values-1"></p>
+            </div>
+            <div class="label">
+                <label for="email">E-mail:</label>
+                <input type="email" name="email" id="input2" placeholder="E-mail address"> 
+                <p id="values-2"></p>
+            </div>
+            <div class="label">
+                <label for="password">Jelszó:</label>
+                <input type="password" name="password" id="input3" placeholder="password">                         
+                <select id="animals" name="animals">
+                    <option value="js">JavaScript</option>
+                    <option value="php">PHP</option>
+                    <option value="c#">Csharp</option>
+                </select>
+                <p id="values-3"></p>
+            </div>
+            <div>
+                <button>Send</button>
+            </div>
+        </form>
+    </section>
+       `);
 
-    console.log(c); // itt 1et kapunk mert itt latszodik a c
+   /*  const inputList = document.querySelectorAll('input'); */
+
+    /* for (const input of inputList) {
+        input.addEventListener("input", function(event){
+            console.log(event.target.value);            
+        })
+    }  */
+
+    //UGYANEZ MAP-EL:
+
+    const inputList = document.querySelectorAll("input");
+
+    Array.from(inputList).map(function(input){
+        input.addEventListener("input", function(event){
+            console.log(event.target.value);   
+        })      
+
+    })
+    //map tombot ad vissza es az az elonye h nem kell utana meg 1 tombot kesziteni amibe belepakolja
+    //querySelectorAllnal kell az Array.from-ot hasznalni mert a querySelectorAll igazabol nem array
+
+    const form = rootElement.querySelector("form")
+
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        console.log(e.target);
+    })
+
+//CSAK A FORMOK ESETEBEN A GOMBOKNAL NEM CLICK ESEMENY KELL HANEM SUBMIT ESEMENY!!!!!!
+
+//select!!! 
+
+    form.querySelector("select").addEventListener("input", function(e){
+        console.log(e.target.value)
+    })
 
 
-}
+}       
 
-init();
-
-//UGYANEZ CSAK ITT AZ A LENYEG H NEM AKARJUK EGYBOL MEGHIVNI ES NEV NELKUILIEK
-
-function init (){
-    var a =  50;
-    var b = 6;
-
-    console.log(c) // undifined lesz ha most lefuttatjuk
-
-    if (a < b){
-       (function (){ //itt lekeretezzuk a var scopjeat mert local scopeja van
-            var c = b - a;
-            console.log(c);
-       
-        })();
-    } else {
-        (function () {
-            var c = a - b;        
-        })();
-    };
-
-    console.log(c); // itt 1et kapunk mert itt latszodik a c
-}
-
-
-
-
-
-
-
-
-function init(mathFunction){
-    var a =  50;
-    var b = 6;
-
-    console.log(c) // undifined lesz ha most lefuttatjuk
-
-    if (a < b){
-        let c =mathFunction(b,a);
-             
-        console.log(c);
-    } else {
-        let c =mathFunction(a,b); 
-
-    console.log(c);
-        
-}
-
-//lenyeg h minket nem erdekel h hivjak 
-//cel az init fuggvenyt atadni valahova 
-
-const initC = (firstNumber, secondNumber) =>{
-    return firstNumber - secondNumber;
-
-};
-
-const initD = (firstNumber,secondNumber) =>{
-    return firstNumber * secondNumber;
-
-};
-
-init(initD);
+     
+  window.addEventListener("load", loadEvent)
